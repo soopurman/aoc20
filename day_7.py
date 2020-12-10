b02910286a7c0e7e220d50e4c06fc343c1e77674
Michael Roberts <mroberts@sysengineers.net>
2020-12-10 17:23:32 -0500

    implemented day 7 part 1

---
+++ day_7.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''Advent of Code 2020'''
+
+import re
+import os
+import argparse
+import browsercookie
+import requests
+
+
+def up(input, targets, holders=None):
+    if not holders:
+        holders = set()
+    new_holders = set()
+    for line in input.text.splitlines():
+        container, contents = line.split('contain')
+        for t in targets:
+            if t in contents:
+                bag = container.split(' bag')[0]
+                if bag not in holders:
+                    new_holders.add(bag)
+    if new_holders:
+        holders.update(new_holders)
+        holders.update(up(input, new_holders, holders))
+    return holders
+
+
+def down(input, target):
+    pass
+
+
+if __name__ == '__main__':
+    puzzle = re.findall(r'[^._]+', os.path.basename(__file__))
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-input', help='URL of puzzle input',
+        default=f"https://adventofcode.com/2020/{puzzle[0]}/{puzzle[1]}/input")
+    parser.add_argument('-target', default='shiny gold', help='type of bag we want to find storage for')
+    parser.add_argument('-down', action='store_true', help='count down the contents of the target (as opposed to up its containers)')
+    args = parser.parse_args()
+
+    jar = browsercookie.load()
+    input = requests.get(args.input, cookies=jar)
+
+    if not args.down:
+        print(len(up(input, {args.target})))
+    else:
+        print(down(input, args.target))
